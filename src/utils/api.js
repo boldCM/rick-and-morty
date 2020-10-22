4bc40087123e18d46beb4bc1f03fe502068f3eff
boldCM <carolin@cc-meier.de>
2020-10-22 11:23:35 +0200

    Add explaining comments on API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,15 @@
 export async function getCharacterById(id) {
   const url = `https://rickandmortyapi.com/api/character/${id}`;
   const response = await fetch(url);
+  // await sagt Javascript, es soll warten, bis die Antwort da ist.
+  // Die funktion pausiert mit ihrer Ausführung in der entsprechenden await Zeile,
+  // bis die Antwort (=promise = ich hab dir doch gesagt, dass ich mich darum kümmere!) da ist
+  // Das geht auch nicht so sehr auf die CPU, weil JS in der Zeit andere Sachen tun kann
   const character = await response.json();
+  // ist character unser promise? also das Waren auf die Antwort?
+  // ich glaube promise ist immer mit einem Alternativ-weg wie .then() oder .catch() verbunden...
+  // async functions always return a promise (MDN)
+  // das keyword async sorgt dafür, dass ein promise returned wird (?) auch ohne return statement?
   return character;
 }
 
