62e7b2184726eb31786db46c05e5e0fed7034978
boldCM <carolin@cc-meier.de>
2020-10-22 11:44:18 +0200

    Update API-file

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,14 +13,14 @@ export async function getCharacterById(id) {
   return character;
 }
 
-export async function getCharacter(name) {
-  let url = "https://rickandmortyapi.com/api/character/";
+export async function getCharacter(name, page = 1) {
+  let url = "https://rickandmortyapi.com/api/character/?page=${page}";
   if (name) {
-    url += `?name=${name}`;
+    url += `&name=${name}`;
   }
 
   const response = await fetch(url);
   const data = await response.json();
   // const fstCharacters = data.results.slice();
-  return data.results;
+  return data;
 }
