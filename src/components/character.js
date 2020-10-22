5e3839b00f812155ad5302e4fe808d2883c7e86e
boldCM <carolin@cc-meier.de>
2020-10-22 17:51:53 +0200

    Create and place Star Icon

--- src/components/character.js
+++ src/components/character.js
@@ -1,9 +1,16 @@
 import { createElement } from "../utils/elements";
 import "../components/character.css";
+import starActive from "../assets/star_active.svg";
+import starInactive from "../assets/star_inactive.svg";
 
-export const createCharacterCard = ({ name, avatar }) => {
+export const createCharacterCard = ({ name, avatar, favouritesActive }) => {
+  const favouritesIcon = createElement("img", {
+    className: "characterCard__header__favourite",
+    src: favouritesActive ? starActive : starInactive,
+  });
+
   const characterName = createElement("div", {
-    className: "characterCard__header",
+    className: "characterCard__header__name",
     innerText: name,
   });
 
@@ -14,9 +21,14 @@ export const createCharacterCard = ({ name, avatar }) => {
     loading: "lazy",
   });
 
+  const characterCardHeader = createElement("div", {
+    className: "characterCard__header",
+    children: [favouritesIcon, characterName],
+  });
+
   const characterCard = createElement("div", {
     className: "characterCard",
-    children: [characterName, characterImg],
+    children: [characterCardHeader, characterImg],
   });
 
   return characterCard;
